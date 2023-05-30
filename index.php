d0295e955a850f80352bfcb3586ca002375c5098
Carlos Ferrer <carlosferrermartinez11@gmail.com>
2023-05-30 11:40:17 +0200

    mensajes añadidos

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+//obtenemos valores enviados del formulario
+if (count($_POST) > 0) {
+    $nombre = $_POST['name'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['message'];
+    //configuramos detalles del envio del correo
+    $destinatario = 'carlosferrermartinez11@gmail.com';
+    $asunto = 'Nuevo mensaje de contacto (portfolio)';
+
+    // Creamos el cuerpo del correo
+    $cuerpo = "Nombre: $nombre\n";
+    $cuerpo .= "Email: $email\n";
+    $cuerpo .= "Mensaje:\n$mensaje\n";
+
+    // Envíamos el correo funcion 'mail'
+    if (mail($destinatario, $asunto, $cuerpo)) {
+        echo 'Mensaje enviado con éxito. Gracias por contactarnos.';
+    } else {
+        echo 'Error al enviar el mensaje. Por favor, intenta nuevamente.';
+    }
+}
+
+/* var_dump($_POST); */
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,8 +35,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CARLOS FERRER MARTÍNEZ</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -112,7 +140,7 @@
         </div>
         <div class="container">
             <div class="row">
-                <div class="col-lg-4">
+                <div class="col-lg-4 col-sm-12">
                     <div class="card text-center" style="width:400px">
                         <img class="card-img-top" src="assets/images/phoneswap.png" alt="Card image">
                         <div class="card-body">
@@ -124,7 +152,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 offset-lg-4">
+                <div class="col-lg-4 offset-lg-4 col-sm-12">
                     <div class="card text-center" style="width:400px">
                         <img class="card-img-top" src="assets/images/portfolio.png" alt="Card image">
                         <div class="card-body">
@@ -140,19 +168,50 @@
         </div>
     </section>
 
+    <section>
+        <div class="container">
+            <div class="row text-center">
+                <div class="col">
+                    <h1>Contact</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container formulario d-flex justify-content-center align-items-center">
+            <div class="row">
+                <div class="col-lg">
+                    <form action="" method="post">
+                        <label for="nombre">Nombre:</label>
+                        <input type="text" id="nombre" name="name" required><br>
+
+                        <label for="email">Email:</label>
+                        <input type="email" id="email" name="email" required><br>
+
+                        <label for="mensaje">Mensaje:</label><br>
+                        <textarea id="mensaje" name="message" rows="4" required></textarea><br>
+
+                        <input type="submit" value="Enviar">
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-lg-3">
+                <div class="col-lg-4">
                     <p>My Skills</a> icons by <a target="_blank" href="https://icons8.com">Icons8</a></p>
                 </div>
+                <div class="col-lg-4 offset-lg-4">
+                <h3>Carlos Ferrer Martínez</h3>
+                <p>Todos los derechos reservados &copy; 2023</p>
+            </div>
             </div>
         </div>
     </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
